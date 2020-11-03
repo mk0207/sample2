ba87ea0074a3c0f5ee5509e1849a67cd3153f874
mk0207 <matumototenpa@gmail.com>
2020-11-03 23:04:54 +0900

    不要ファイル生成しない設定

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module Sample2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
