760adb483b85c5d5f0b1286c86b698b75668b659
mk0207 <matumototenpa@gmail.com>
2020-11-05 21:23:37 +0900

    修正

--- db/migrate/20201105121610_create_messages.rb
+++ db/migrate/20201105121610_create_messages.rb
@@ -1,9 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-       t.string :text, null: false
-       t.references :user, foreign_key: true
-
+      t.string :text, null: false
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
