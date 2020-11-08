5f0147f27e7bcd88359a5aee2d525cd1771bfa16
mk0207 <matumototenpa@gmail.com>
2020-11-08 22:04:56 +0900

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :messages, only: [:index]
+  resources :messages, only: [:index, :create]
   resources :users
 end
